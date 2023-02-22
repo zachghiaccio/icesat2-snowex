2cddc6c0ccc93210332d2956f29d2acef00f9642
zachghiaccio <zhfair@umich.edu>
2023-02-22 20:18:19 +0000

    Cleaned up KML script and added more commentary.

--- is2_kml_maker.py
+++ is2_kml_maker.py
@@ -13,8 +13,10 @@ from osgeo import ogr
 
 def is2_kml_maker(is2_file, beam):
     """
-    A quick script to convert ICESat-2 lat/lon coordinates into a KML file.
+    Converts ICESat-2 lat/lon coordinates into a KML file.
     Needs a beam specification.
+    
+    **Requires the "simplekml" Python package.***
 
     Parameters
     ----------
@@ -22,7 +24,7 @@ def is2_kml_maker(is2_file, beam):
         The .h5 file containing ICESat-2 data. Currently only works with
         ATL08 data.
     beam: str
-        The beam data to be read in. Accepts: gt1r, gt1l, gt2r, gt2l, gt3r, gt3l
+        The beam data to be read. Accepts: gt1r, gt1l, gt2r, gt2l, gt3r, gt3l
 
     Returns
     -------
@@ -50,6 +52,7 @@ def is2_kml_maker(is2_file, beam):
     for row in coords:
         ls.coords.addcoordinates([(row[0], row[1])])
         
+    # Save the new KML file
     rgt = is2_file[91:95]
     is2_kml.save('rgt%s_%s.kml' %(rgt, beam))
     
@@ -57,9 +60,31 @@ def is2_kml_maker(is2_file, beam):
 
 #\---------------------------------------------------------/#
 def kml_to_coords(kml_file):
+    """
+    Takes the lat/lon coordinates from an ICESat-2 KML file
+    and places them into numpy arrays.
     
+    Parameters
+    ----------
+    kml_file: str
+        A KML file containing ICESat-2 coordinate information.
+        KMLs from the above function and from the ICESat-2
+        website are both valid.
+        
+    Returns
+    -------
+    kml_lat: numpy array
+        Array of latitudes for ICESat-2 track.
+    kml_lon: numpy array
+        Array of longitudes for ICESat-2 track.
+        
+    """
+    
+    # Open the KML file with osgeo
     ds = ogr.Open(kml_file)
     
+    # Loop through the points within the KML file, and
+    # add them to an array
     coords = []
     for lyr in ds:
         for feat in lyr:
@@ -70,6 +95,7 @@ def kml_to_coords(kml_file):
                     
     coords = np.array(coords)
     
+    # Separate the latitudes and longitudes into individual arrays
     kml_lat = coords[:,1]
     kml_lon = coords[:,0]
     
@@ -77,41 +103,64 @@ def kml_to_coords(kml_file):
 
 #\---------------------------------------------------------/#
 def make_new_rgts(kml_file):
+    """
+    Takes KMLs from the ICESat-2 website, or center beams 
+    generated from is2_kml_maker, to approximate the left
+    and right beam paths. The paths are saved into separate
+    KML files.
     
+    **Requires the "simplekml" Python package.***
+    
+    Parameters
+    ----------
+    kml_file: str
+        A KML file containing ICESat-2 coordinate information.
+        KMLs from the above function and from the ICESat-2
+        website are both valid.
+        
+    Returns
+    ----------
+    kml_left: KML file
+        KML track for left beam(s).
+    kml_right: KML file
+        KML track for right beam(s).
+        
+    """
+    
+    # Convert KML coordinates to numpy arrays
     kml_lat,kml_lon = kml_to_coords(kml_file)
     
+    # Estimate location of left and right beams, given
+    # a beam pair spacing of ~3.39 km
     dellon = 3390./(np.cos(np.radians(kml_lat))*111000)
-    lon_3l = kml_lon + dellon
-    lon_1l = kml_lon - dellon
-    #raise ValueError('debug')
-    kml_gt1l = simplekml.Kml()
-    kml_gt3l = simplekml.Kml()
+    lon_right = kml_lon + dellon
+    lon_left = kml_lon - dellon
+ 
+    # Initialize new KML files
+    kml_right = simplekml.Kml()
+    kml_left = simplekml.Kml()
     
-    ls3l = kml_gt3l.newlinestring(name='gt3l')
-    ls1l = kml_gt1l.newlinestring(name='gt1l')
+    # Assign labels to KMLs
+    ls_right = kml_right.newlinestring(name='left beam')
+    ls_left = kml_left.newlinestring(name='right beam')
     
-    coords_gt3l = np.empty([len(lon_3l), 2])
-    coords_gt1l = np.empty([len(lon_1l), 2])
+    # Assign coordinates to 2D arrays
+    coords_right = np.empty([len(lon_left), 2])
+    coords_left = np.empty([len(lon_right), 2])
     
-    coords_gt3l[:,0],coords_gt3l[:,1] = lon_3l,kml_lat
-    coords_gt1l[:,0],coords_gt1l[:,1] = lon_1l,kml_lat
+    coords_right[:,0],coords_right[:,1] = lon_right,kml_lat
+    coords_left[:,0],coords_left[:,1] = lon_left,kml_lat
     
-    #raise ValueError('debug')
-    for row in coords_gt3l:
-        ls3l.coords.addcoordinates([(row[0], row[1])])
+    # Add lat/lon coordinates to KMLs
+    for row in coords_right:
+        ls_right.coords.addcoordinates([(row[0], row[1])])
         
-    for row in coords_gt1l:
-        ls1l.coords.addcoordinates([(row[0], row[1])])
+    for row in coords_left:
+        ls_left.coords.addcoordinates([(row[0], row[1])])
     
-    rgt = kml_file[102:106]
-    try:
-    
-        kml_gt1l.save('C:/Users/zfair/OneDrive - NASA/Documents/Python/icesat2-snow/rgt%s_gt1l' %(rgt))
-        kml_gt3l.save('C:/Users/zfair/OneDrive - NASA/Documents/Python/icesat2-snow/rgt%s_gt3l' %(rgt))
-    except:
-        rgt = kml_file[113:117]
-        
-        kml_gt1l.save('C:/Users/zfair/OneDrive - NASA/Documents/Python/icesat2-snow/rgt%s_gt1l' %(rgt))
-        kml_gt3l.save('C:/Users/zfair/OneDrive - NASA/Documents/Python/icesat2-snow/rgt%s_gt3l' %(rgt))
+
+    # Save the new KML files
+    kml_right.save('approx_right_beams.kml')
+    kml_left.save('approx_left_beams.kml)
     
     return kml_gt3l, kml_gt1l
